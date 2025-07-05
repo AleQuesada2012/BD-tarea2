8eb08d269751834e38658da38d04007cdab5029a
AleQuesada2012 <ale11199@estudiantec.cr>
2025-07-05 13:18:23 -0600

    Add calls to helper logic
    also added consulted links to the .txt file

--- programaestudiante.py
+++ programaestudiante.py
@@ -3,9 +3,9 @@ import sys
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-import funciones 
+import funciones as funcs
 import json
-
+import generar_csv
 
 spark = SparkSession.builder.appName("Tarea-2-Big Data").getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
@@ -27,16 +27,25 @@ for file in sys.argv[1:]:
         except json.JSONDecodeError:
             print(f"El archivo {file} es un JSON de formato no valido")        
         current_json.close()
-
+# funciona, todo: separarlos para que quede tabular  
 
 dataframes = []
 for json_file in json_list:
     df = spark.read.option("multiLine", "true").json(json_file)
     dataframes.append(df)
 
-
+normalizados = []
 for datafa in dataframes:
-    datafa.printSchema()
-    datafa.show(truncate=False)
+    newDF = funcs.normalizar(datafa)
+    normalizados.append(newDF)
+    #newDF.show(truncate=False)
 
-# funciona, todo: separarlos para que quede tabular    
+
+
+df_final = funcs.unificar(normalizados)
+df_final.printSchema()
+df_final.show(df_final.count())
+
+print(f"cantidad de compras: {df_final.count()}")
+
+generar_csv.total_productos(df_final)
